41397bc5caaeccff40c7739ffa49273b5a76717e
Raghu Betina <raghu@firstdraft.com>
2017-02-24 21:20:41 +0000

    Direct association between users and pantry_items

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :pantry_items,
+             :dependent => :destroy
+
   has_many   :ratings,
              :dependent => :destroy
 
