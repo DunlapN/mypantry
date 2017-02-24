cf41f1dae2989aad1396450d6a5adf689668f806
Raghu Betina <raghu@firstdraft.com>
2017-02-24 21:21:09 +0000

    Whitelist columns in Recipe for ActiveAdmin

--- app/admin/recipe.rb
+++ app/admin/recipe.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Recipe do
 
+ permit_params :name, :creator, :description, :instructions, :nutritional_facts, :image
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
